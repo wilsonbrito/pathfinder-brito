9d68740b23320c1a9a72d3d94be3d6a7abd38205
wilsonbrito <wilsonjb93@gmail.com>
2018-05-10 14:56:21 -0300

    qui, 10 de mai, 14:56

--- parse-xml.js
+++ parse-xml.js
@@ -1,22 +1,24 @@
 var fs = require('fs');
 var parse = require('xml-parser');
-fs.readFile('marilia-pequena.osm', 'utf8', function(err, xml){
+fs.readFile('map.osm', 'utf8', function(err, xml){
   var inspect = require('util').inspect;
 
   var obj = parse(xml);
 
+  //imprime todos objetos
   //console.log(inspect(obj, { colors: true, depth: Infinity }));
+
+  //descobrir quantos nó irei percorrer
   //console.log(obj.root.children.length)
 
   for(let i = 0; i <= obj.root.children.length; i++){
-
-    g.addnod(
-      obj,name,
-      obj,numero
-    ){
-
-    }
-    console.log(obj.root.children[i].attributes.id);
+  //   // g.addnod(
+  //   //   obj,name,
+  //   //   obj,numero
+  //   // )
+  //   // }
+  
+    console.log(obj.root.children[i].attributes);
   }
   // var cloneAndPluck = function(sourceObject, keys) {
   //     var newObject = {};
--- xmldom.js
+++ xmldom.js
@@ -1,13 +1,20 @@
-var DOMParser = require('xmldom').DOMParser;
-var doc = new DOMParser().parseFromString(
-    '<?xml version="1.0" encoding="UTF-8"?>\n'+
-        '\t<child>test</child\n'+
-        '\t<child></child>\n'+
-        '\t<child/>\n'+
-    '</xml>'
-    ,'text/xml');
-doc.documentElement.setAttribute('x','y');
-doc.documentElement.setAttributeNS('./lite','c:x','y2');
-var nsAttr = doc.documentElement.getAttributeNS('./lite','x')
-console.info(nsAttr)
-console.info(doc)
+// var DOMParser = require('xmldom').DOMParser;
+// var doc = new DOMParser().parseFromString(
+//     '<?xml version="1.0" encoding="UTF-8"?>\n'+
+//         '\t<child>test</child\n'+
+//         '\t<child></child>\n'+
+//         '\t<child/>\n'+
+//     '</xml>'
+//     ,'text/xml');
+// doc.documentElement.setAttribute('x','y');
+// doc.documentElement.setAttributeNS('./lite','c:x','y2');
+// var nsAttr = doc.documentElement.getAttributeNS('./lite','x')
+// console.info(nsAttr)
+// console.info(doc)
+var fs = require('fs');
+var DomParser = require('xml-parser');
+fs.readFile('marilia-pequena.osm', 'utf8', function(err, xml){
+  var parser = new DomParser();
+  var doc = parser.parseFromString(xml, "application/xml");
+  console.log(doc);
+});

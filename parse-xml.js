a6abc59d38d448ebf802cfe3e9469c1d2a562788
wilsonbrito <wilsonjb93@gmail.com>
2018-05-10 13:21:32 -0300

    qui, 10 de mai, 13:21

--- parse-xml.js
+++ parse-xml.js
@@ -6,16 +6,27 @@ fs.readFile('marilia-pequena.osm', 'utf8', function(err, xml){
   var obj = parse(xml);
 
   //console.log(inspect(obj, { colors: true, depth: Infinity }));
+  //console.log(obj.root.children.length)
 
-  var cloneAndPluck = function(sourceObject, keys) {
-      var newObject = {};
-      keys.forEach(function(key) { newObject[key] = sourceObject[key]; });
-      return newObject;
-  };
+  for(let i = 0; i <= obj.root.children.length; i++){
 
-  var subset = cloneAndPluck(obj, ["lat", "lon"]);
+    g.addnod(
+      obj,name,
+      obj,numero
+    ){
 
-  console.log(subset);
-
-  //https://stackoverflow.com/questions/17781472/how-to-get-a-subset-of-a-javascript-objects-properties
+    }
+    console.log(obj.root.children[i].attributes.id);
+  }
+  // var cloneAndPluck = function(sourceObject, keys) {
+  //     var newObject = {};
+  //     keys.forEach(function(key) { newObject[key] = sourceObject[key]; });
+  //     return newObject;
+  // };
+  //
+  // var subset = cloneAndPluck(obj, ["lat", "lon"]);
+  //
+  // console.log(subset);
+  //
+  // //https://stackoverflow.com/questions/17781472/how-to-get-a-subset-of-a-javascript-objects-properties
 });
